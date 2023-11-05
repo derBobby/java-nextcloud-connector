546ea39cc5dbb1a0cf08b3f990d6f6504fa37b38
derBobby <dev@planlos.eu>
2023-11-05 22:50:16 +0100

    Clean up tests

--- src/test/java/eu/planlos/javanextcloudconnector/ContextTest.java
+++ src/test/java/eu/planlos/javanextcloudconnector/ContextTest.java
@@ -1,10 +1,14 @@
 package eu.planlos.javanextcloudconnector;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NextcloudConnectorApplicationTest {
+@ConfigurationPropertiesScan
+@SpringBootApplication
+class ContextTest {
 
     @Test
     void contextLoads() {
