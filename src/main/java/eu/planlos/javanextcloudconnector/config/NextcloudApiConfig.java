d75d4c5cd653fc93090824e8ff1d5d56d5c8d642
derBobby <dev@planlos.eu>
2023-12-13 00:05:01 +0100

    Fix Test configs

--- src/main/java/eu/planlos/javanextcloudconnector/config/NextcloudApiConfig.java
+++ src/main/java/eu/planlos/javanextcloudconnector/config/NextcloudApiConfig.java
@@ -1,12 +1,9 @@
 package eu.planlos.javanextcloudconnector.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Profile;
 
 @ConfigurationProperties(prefix = "nextcloud.api")
-@Profile("!TEST")
 public record NextcloudApiConfig(boolean active, String address, String user, String password, String defaultGroup, int retryCount, int retryInterval, String accountNamePrefix, String accountNameSuffix) {
-
     public boolean inactive() {
         return !active;
     }
